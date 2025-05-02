9635cff67af9bdeb822fe457324bb6877659c000
dhruv27tyagi <dhruv63tyagi@gmail.com>
2025-05-02 23:23:36 +0530

    tkinter

---
+++ Day 27 - TkInter/main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+window = tkinter.Tk()
+window.title("First GUI Program")
+
+#Label 
+
+my_label = tk.Label(text="I am a label")
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
