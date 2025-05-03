864410428d1c2f46910fd73748fb410e77d9e9ba
dhruv27tyagi <dhruv63tyagi@gmail.com>
2025-05-03 23:15:37 +0530

    tkinter

--- Day 27 - TkInter/main.py
+++ Day 27 - TkInter/main.py
@@ -1,21 +1,26 @@
 import tkinter as tk
 
-window = tkinter.Tk()
-window.title("First GUI Program")
+#window = tk.Tk()
+#window.title("First GUI Program")
 
 #Label 
 
-my_label = tk.Label(text="I am a label")
-my_label.pack()
+#my_label = tk.Label(text="I am a label")
+#my_label.pack()
+
+def add(*n):
+    sum = 0
+    for i in n:
+        sum = sum + i
+
+    return sum
+
+
+print(add(5,6,7,8,9))
 
 
 
 
 
 
-
-
-
-
-
-window.mainloop()
+#window.mainloop()
